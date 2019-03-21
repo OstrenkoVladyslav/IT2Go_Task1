5775caa77d02afce68129e710e961759f555dde7
OstrenkoVladyslav <mr.ostrenko@gmail.com>
2019-03-21 15:24:20 -0400

    Working with unit test

---
+++ test/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testSumOfPositiveNumbers(){
+        int resultToTest = Main.sum(33,12);
+        assertEquals(45, resultToTest);
+        resultToTest = Main.sum(21,10);
+        assertEquals(45, resultToTest);
+    }
+}
